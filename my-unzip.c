ae5da250379840bec9c59801f98933c3a6e9840f
Venne <venne@gmail.com>
2026-07-31 19:18:07 +0300

    Final

--- my-unzip.c
+++ my-unzip.c
@@ -9,35 +9,37 @@ This is a run length decoding. Takes in a compressed binary file and returns it 
 Run like: ./my-unzip <input.bin> > output.txt
 */
 int main(int argc, char *argv[]){
-  if(argc != 2){
+  if(argc < 2){
     fprintf(stderr,"my-unzip takes in 1 argument\n");
     exit(1);
   }
 
-  // Opening the read file
-  FILE *fileIn = fopen(argv[1],"rb");
-  if(fileIn == NULL){
-    fprintf(stderr,"my-unzip: cannot open file\n");
-    exit(1);
-  }
-
-  // Reading the file
-  // There are 2 variables used now, the count (How many times character appears in row), and the actual character
-  char character;
-  int count = 0;
-  
-  // The file can be read in pairs, always first reading th count, then the character
-  while((fread(&count,sizeof(int),1,fileIn)) == 1){
-    if(fread(&character, sizeof(char),1,fileIn) != 1){
-      fprintf(stderr,"Error occurred when reading character\n");
-      fclose(fileIn);
+  for (int j = 1; j < argc; j++){  
+    // Opening the read file
+    FILE *fileIn = fopen(argv[j],"rb");
+    if(fileIn == NULL){
+      fprintf(stderr,"my-unzip: cannot open file\n");
       exit(1);
     }
-    // Printing the character to stdout
-    for(int i = 0; i < count; i++){
-      fprintf(stdout,"%c", character);
+
+    // Reading the file
+    // There are 2 variables used now, the count (How many times character appears in row), and the actual character
+    char character;
+    int count = 0;
+    
+    // The file can be read in pairs, always first reading th count, then the character
+    while((fread(&count,sizeof(int),1,fileIn)) == 1){
+      if(fread(&character, sizeof(char),1,fileIn) != 1){
+        fprintf(stderr,"Error occurred when reading character\n");
+        fclose(fileIn);
+        exit(1);
+      }
+      // Printing the character to stdout
+      for(int i = 0; i < count; i++){
+        fprintf(stdout,"%c", character);
+      }
     }
+    fclose(fileIn);
   }
-  fclose(fileIn);
   exit(0);
 }
